6252afb6b8082d67386105c56bb0b4182f49161e
kamiokan <spherenov@gmail.com>
2019-12-18 00:17:22 +0900

    Example 2.15 関数やメソッドの出力内容のテスト

---
+++ OutputTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class OutputTest extends TestCase
+{
+    public function testExceptFooActualFoo()
+    {
+        $this->expectOutputString('foo');
+        print 'foo';
+    }
+
+    public function testExpectBarActualBaz()
+    {
+        $this->expectOutputString('bar');
+        echo 'baz';
+    }
+}
