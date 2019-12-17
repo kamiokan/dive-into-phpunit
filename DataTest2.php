904ba27bb218977607e7b1a9e0985677eace68a5
kamiokan <spherenov@gmail.com>
2019-12-17 17:53:41 +0900

    Example 2.10 ひとつのテストでの複数のデータプロバイダの使用

---
+++ DataTest2.php
@@ -0,0 +1,33 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DataTest2 extends TestCase
+{
+    /**
+     * @dataProvider additionWithNonNegativeNumbersProvider
+     * @dataProvider additionWithNegativeNumbersProvider
+     */
+    public function testAdd($a, $b, $expected)
+    {
+        $this->assertSame($expected, $a + $b);
+    }
+
+    public function additionWithNonNegativeNumbersProvider()
+    {
+        return [
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 3],
+        ];
+    }
+
+    public function additionWithNegativeNumbersProvider()
+    {
+        return [
+            [-1, -1, 0],
+            [-1, -1, -2],
+            [1, -1, 0],
+        ];
+    }
+}
