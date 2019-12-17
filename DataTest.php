e4ea9a32a8bd7bde46b4c991306a68f0a6e06a36
kamiokan <spherenov@gmail.com>
2019-12-17 17:14:19 +0900

    Example 2.6 データプロバイダでの名前つきデータセットの使用

--- DataTest.php
+++ DataTest.php
@@ -15,10 +15,10 @@ class DataTest extends TestCase
     public function additionProvider()
     {
         return [
-            [0, 0, 0],
-            [0, 1, 1],
-            [1, 0, 1],
-            [1, 2, 3],
+            'adding zeros'  => [0, 0, 0],
+            'zero plus one' => [0, 1, 1],
+            'one plus zero' => [1, 0, 1],
+            'one plus one'  => [1, 1, 3],
         ];
     }
 
