8f1fb58d5d65749da2362c681187118a0163d17e
kamiokan <spherenov@gmail.com>
2019-12-17 16:08:59 +0900

    Example 2.2 @depends アノテーションを使った依存性の表現

--- StackTest.php
+++ StackTest.php
@@ -4,6 +4,39 @@ use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
 {
+    public function testEmpty()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     * @param array $stack
+     * @return array
+     */
+    public function testPush(array $stack)
+    {
+        $this->assertEmpty($stack, 'なんてことだ！前のテストの戻り値をそのまま引き継げるとは！');
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack) - 1]);
+        $this->assertNotEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testPush
+     * @param array $stack
+     */
+    public function testPop(array $stack)
+    {
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertEmpty($stack);
+    }
+
     public function testPushAndPop()
     {
         $stack = [];
