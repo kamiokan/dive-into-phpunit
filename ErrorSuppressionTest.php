7867134fb45e2ffc6dfcbf01bcc6636f0c670865
kamiokan <spherenov@gmail.com>
2019-12-18 00:12:23 +0900

    Example 2.14 PHP のエラーが発生するコードの返り値のテスト

---
+++ ErrorSuppressionTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ErrorSuppressionTest extends TestCase
+{
+    public function testFileWriting()
+    {
+        $writer = new FileWriter;
+        $this->assertFalse(@$writer->write('/is-not-writable/file', 'stuff'));
+    }
+}
+
+class FileWriter
+{
+    public function write($file, $content)
+    {
+        $file = fopen($file, 'w');
+        if ($file == false) {
+            return false;
+        }
+
+        // ...
+    }
+}
